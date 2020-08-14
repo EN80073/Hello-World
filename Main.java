df9bf7c8cc09c5da66deaeb53ad97b10ce7bc4c3
EN80073 <69617819+EN80073@users.noreply.github.com>
2020-08-14 10:50:37 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Edison.");
+		System.out.println("I play games.");
+		System.out.println("I have a pet bird.");
+		System.out.println("I skipped Second Grade.");
+		System.out.println("I am cool. B)");
+	}
+}
